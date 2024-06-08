d6d35c6bd6517291bd1da9fee6eaba1417744e91
Ten <03822007sasha@gmail.com>
2024-06-08 03:18:29 +0300

    init

--- miss_animals/models.py
+++ miss_animals/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from saitInfaProject import settings
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class FoundAnimalCard(models.Model):
+    image = models.ImageField(upload_to=settings.MEDIA_URL)
+    area = models.CharField(max_length=100)
+    place = models.CharField(max_length=200)
+    contact = PhoneNumberField()
+    date = models.DateField()
+
+    ADDED_CHOICES = [
+        ("Пользователь зарегистрирован", "YesReg"),
+        ("Пользователь не зарегистрирован", "NoReg")
+    ]
+    added = models.CharField(max_length=50, choices=ADDED_CHOICES)
--- miss_animals/views.py
+++ miss_animals/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import FoundAnimalCard
 
 
 def home_page(request):
-    return render(request, "miss_animals/home_page.html")
+    found_animal_card = FoundAnimalCard.objects.all()
+    return render(request, "miss_animals/home_page.html", {"found_animal_cards": found_animal_card})
 
 
 def search_page(request):
